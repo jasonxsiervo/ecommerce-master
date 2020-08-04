1503e044f7ed9dbe77cbe6b0960ff1f81225d1cd
jasonxsiervo <43787504+jasonxsiervo@users.noreply.github.com>
2020-08-04 19:33:36 +0800

    animating cart count component

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e79df8a3a57ebcd2993b";
+/******/ 	var hotCurrentHash = "c9ed61d561528c391960";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
