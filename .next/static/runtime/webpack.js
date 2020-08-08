62fcf33cfd5e91e810b6c922f4a6959cf65b5dbe
jasonxsiervo <43787504+jasonxsiervo@users.noreply.github.com>
2020-08-09 00:01:17 +0800

    saving orders to the database

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "82ad80233b77646c48f6";
+/******/ 	var hotCurrentHash = "6fbe4453471393d55276";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
