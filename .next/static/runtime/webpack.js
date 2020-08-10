63c8177f0d31386563047bfb1281e1a5d846a839
jasonxsiervo <43787504+jasonxsiervo@users.noreply.github.com>
2020-08-10 22:31:00 +0800

    displaying single orders and adding orders page

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "136f136a0323884005fa";
+/******/ 	var hotCurrentHash = "a3d5eefbe573bd96855c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
